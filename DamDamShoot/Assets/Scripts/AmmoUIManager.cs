1175fd0f39e2efe79143b5be7b0185c620b3378f
GHOSTVCL <joanetgiolet@gmail.com>
2024-11-21 21:33:11 +0100

    Canvi xorra

--- DamDamShoot/Assets/Scripts/AmmoUIManager.cs
+++ DamDamShoot/Assets/Scripts/AmmoUIManager.cs
@@ -28,7 +28,7 @@ public class AmmoUIManager : MonoBehaviour
             UpdateBullets(p1BulletContainer, player1.ammoCount);
         }
 
-        // Update bullet icons for Player 2
+     
         if (player2 != null)
         {
             UpdateBullets(p2BulletContainer, player2.ammoCount);
@@ -37,13 +37,13 @@ public class AmmoUIManager : MonoBehaviour
 
     void UpdateBullets(Transform container, int ammoCount)
     {
-        // Clear existing icons
+      
         foreach (Transform child in container)
         {
             Destroy(child.gameObject);
         }
 
-        // Add new icons
+      
         for (int i = 0; i < ammoCount; i++)
         {
             Instantiate(bulletIconPrefab, container);
