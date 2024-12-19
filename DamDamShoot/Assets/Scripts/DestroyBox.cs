bc58da44a7884d6d76c28dfdeb2dc43bf338888f
Xarderos <99949891+Xarderos@users.noreply.github.com>
2024-12-19 02:02:33 +0100

    wad
    d

--- DamDamShoot/Assets/Scripts/DestroyBox.cs
+++ DamDamShoot/Assets/Scripts/DestroyBox.cs
@@ -10,34 +10,30 @@ public class DestroyBox : MonoBehaviour
         Debug.Log($"Collision detected with: {collision.gameObject.name}");
 
 
-        if (collision.gameObject.CompareTag("BulletP1"))
-            if (collision.gameObject.CompareTag("BulletP1") || collision.gameObject.CompareTag("PowerfulBulletP1"))
+        if (collision.gameObject.CompareTag("BulletP1") || collision.gameObject.CompareTag("PowerfulBulletP1"))
+        {
+            if (CompareTag("Box2"))
             {
-                if (CompareTag("Box2"))
-                {
-                    HandleDestruction(collision.gameObject);
-                }
-                else if (CompareTag("Box1"))
-                {
-                    DestroyBulletOnly(collision.gameObject);
-                }
+                HandleDestruction(collision.gameObject);
             }
+            else if (CompareTag("Box1"))
+            {
+                DestroyBulletOnly(collision.gameObject);
+            }
+        }
 
-            else if (collision.gameObject.CompareTag("BulletP2"))
         else if (collision.gameObject.CompareTag("BulletP2") || collision.gameObject.CompareTag("PowerfulBulletP2"))
-                    {
-                        if (CompareTag("Box1"))
-                        {
-                            HandleDestruction(collision.gameObject);
-                            HandleDestruction(collision.gameObject);
-                        }
-                        else if (CompareTag("Box2"))
-                        {
-                            DestroyBulletOnly(collision.gameObject);
-                        }
-                    }
+        {
+            if (CompareTag("Box1"))
+            {
+                HandleDestruction(collision.gameObject);
+            }
+            else if (CompareTag("Box2"))
+            {
+                DestroyBulletOnly(collision.gameObject);
+            }
+        }
     }
-
     private void HandleDestruction(GameObject bullet)
     {
         Debug.Log("Destruction conditions met. Destroying box...");
